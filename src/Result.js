89d89a80f52281735ebbbd4709a5e400898ac67f
iwenki <139893979+iwenki@users.noreply.github.com>
2024-07-24 14:54:43 -0500

    Added slice method to Results.js

--- src/Result.js
+++ src/Result.js
@@ -8,11 +8,15 @@ export default function Result(props) {
       <div className="Results">
         <h2>Definition of {props.results.word}</h2>
         <h5>{props.results.phonetic}</h5>
-        {props.results.meanings.map(function (meaning, index) {
-          return (<div key={index}>
-            <Meaning meaning={meaning}/>
-            </div>);
-        })}
+        {props.results.meanings
+          .map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })
+          .slice(0, 5)}
       </div>
     );
   } else {
